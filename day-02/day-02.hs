4bb51e89ab79e68972b888ef724d0b47ab7d8a81
Thomas Staud <thomas.staud05@gmail.com>
2025-12-02 11:32:01 +0100

    day 02 part 1 done

--- day-02/day-02.hs
+++ day-02/day-02.hs
@@ -13,7 +13,14 @@ process = let
     parse :: String -> (String, String)
     parse str = let
         (fst, snd) = span (/= '-') str
-        in (fst, drop 1 snd)
+        lenLower = length fst
+        lenUpper = length snd - 1 -- minus is still included
+        -- turns 90,110 into 90,99 and 990,1100 into 1000,1100
+        clean (fst, snd)
+            | lenLower == lenUpper = (fst, drop 1 snd)
+            | even lenLower = (fst, replicate lenLower '9')
+            | otherwise = ('1':replicate (lenUpper-1) '0', drop 1 snd)
+        in clean (fst, snd)
     in sum . map (scanRange . parse)
 
 
@@ -21,14 +28,29 @@ mapTuple :: (a -> b) -> (a, a) -> (b, b)
 mapTuple f (x, y) = (f x, f y)
 
 scanRange :: (String, String) -> Int
-scanRange (lower, upper) = let
+scanRange (lower, upper)
+    | odd $ length lower = 0
+    | head <= tail = concatFactor * ((tail-head+1) * (head+tail)) `div` 2
+    | otherwise = 0
+    where
+        partLength = length lower `div` 2
         parse :: String -> (Int, Int)
-        parse str = mapTuple read $ splitAt (length str `div` 2) str
+        parse str = mapTuple read $ splitAt partLength str
         (fstLower, sndLower) = parse lower
         (fstUpper, sndUpper) = parse upper
-    in (if fstLower >= sndLower then 1 else 0)
-        + (if fstUpper <= sndUpper then 1 else 0)
-        + (fstUpper - fstLower - 1)
+        concatFactor = read $ '1' : replicate (partLength - 1) '0' ++ ['1']
+
+        head = fstLower + if fstLower < sndLower then 1 else 0
+        tail = fstUpper - if fstUpper > sndUpper then 1 else 0
+
+{- generating invalid ids:
+    - for the lower bound, check if it would be in range (store x)
+    - for the upper bound, check if it would be in range (store y)
+    - if yes to both, xx + (x+1)(x+1) + ... + (y-1)(y-1) + yy is the result
+    - otherwise, exclude xx and/or yy
+
+    123 + 124 + 125 = ((125-123+1) * (123+125)) /2
+-}
 
 {- strategy:
     - e.g. 123456 - 126789
