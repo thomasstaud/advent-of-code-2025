94f07461af0f9cac2fe7fc62db9e5edb1b146ea7
Thomas Staud <thomas.staud05@gmail.com>
2025-12-03 14:43:05 +0100

    day 03

--- day-03/day-03.hs
+++ day-03/day-03.hs
@@ -1,13 +1,18 @@
 import Data.Char (ord, chr)
 
-part1 = sum . map process . lines
+part1 = sum . map (process 2) . lines
+part2 = sum . map (process 12) . lines
 
-process :: String -> Int
-process str = let
+process :: Int -> String -> Int
+process n str = let
     xs = map (\ c -> ord c - ord '0') str
-    -- apply function f on xs, then find maximum excluding first index
-    findMax f = maximum . drop 1 . f $ xs
-    digit1 = findMax reverse
-    digit2 = findMax $ dropWhile (/= digit1)
-    in 10 * digit1 + digit2
+    conc = foldr ((:) . (\ x -> chr $ x + ord '0')) []
+    in read . conc . digits xs $ n
+
+digits :: [Int] -> Int -> [Int]
+digits xs 0 = []
+digits xs n = let
+    digit = maximum . drop (n-1) . reverse $ xs
+    remXs = drop 1 . dropWhile (/= digit) $ xs
+    in digit : digits remXs (n-1)
 
