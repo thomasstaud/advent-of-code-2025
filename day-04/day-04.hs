6cb67054b0d8fd4263f7077642785494efd46267
Thomas Staud <thomas.staud05@gmail.com>
2025-12-04 14:05:42 +0100

    day 04 part 1 optimized

--- day-04/day-04.hs
+++ day-04/day-04.hs
@@ -1,20 +1,44 @@
-part1 = process . parse
+part1 str = let
+    mat = parse str
+    in eval (stripMat mat) . mapToAdj $ mat
 
-type Coords = (Int, Int)
+-- paper stack matrix with extended edges
+parse :: String -> [[Bool]]
+parse str = [replicate width False]
+    ++ [[c == '@' | c <- '.':line++['.']] | line <- lines str]
+    ++ [replicate width False]
+    where
+        width = (length . takeWhile (/= '\n')) str + 2
 
--- list of paper stack coords
-parse :: String -> [Coords]
-parse str = [(x, y) | (line, y) <- zip (lines str) [0..], (c, x) <- zip line [0..], c == '@']
+{- strategy:
+    - go through area in 9x9 patches
+    - check each patch and map
+-}
+-- maps each coord to the number of adjacent paper stacks
+mapToAdj :: [[Bool]] -> [[Int]]
+mapToAdj mat = map countAdj $ expandMat mat
 
--- number of accessible paper stacks
-process :: [Coords] -> Int
-process coords = length . filter (isAccessible coords) $ coords
+expandMat :: [[Bool]] -> [([Bool], [Bool], [Bool])]
+expandMat mat = [(t,m,b) |
+    (t, i) <- zip (reverse . drop 2 . reverse $ mat) [0..],
+    let m = mat !! (i+1),
+    let b = mat !! (i+2)]
 
-isAccessible :: [Coords] -> Coords -> Bool
-isAccessible coords c = 4 > (length . filter (isAdjacent c)) coords
+-- remove outer rows/cols
+stripMat :: [[a]] -> [[a]]
+stripMat = let
+    strip = reverse . drop 1 . reverse . drop 1
+    in map strip . strip
 
-isAdjacent :: Coords -> Coords -> Bool
-isAdjacent (x1, y1) (x2, y2) = let
-    xDiff = abs (x1 - x2)
-    yDiff = abs (y1 - y2)
-    in xDiff <= 1 && yDiff <= 1 && (xDiff == 1 || yDiff == 1)
+-- checks how many of the adjacent tiles have paper
+countAdj :: ([Bool], [Bool], [Bool]) -> [Int]
+countAdj ([t1, t2, t3], [m1, m2, m3], [b1, b2, b3]) =
+    [length . filter id $ [t1, t2, t3, m1, m3, b1, b2, b3]]
+countAdj (t1:t2:t3:ts, m1:m2:m3:ms, b1:b2:b3:bs) =
+    (length . filter id $ [t1, t2, t3, m1, m3, b1, b2, b3])
+    : countAdj (t2:t3:ts, m2:m3:ms, b2:b3:bs)
+
+eval :: [[Bool]] -> [[Int]] -> Int
+eval mat adj = let
+    isAccessible (isStack, adj) = isStack && adj < 4
+    in sum [length acc | (matRow, adjRow) <- zip mat adj, let acc = filter isAccessible (zip matRow adjRow)]
