6c72b8daabacb58e9d09304861d487a89808e70e
thomasstaud <thomas.staud05@gmail.com>
2025-12-05 22:47:02 +0100

    improved day 03

--- day-03/day-03.hs
+++ day-03/day-03.hs
@@ -1,18 +1,14 @@
 import Data.Char (ord, chr)
 
-part1 = sum . map (process 2) . lines
-part2 = sum . map (process 12) . lines
+part1 = process 2
+part2 = process 12
 
 process :: Int -> String -> Int
-process n str = let
-    xs = map (\ c -> ord c - ord '0') str
-    conc = foldr ((:) . (\ x -> chr $ x + ord '0')) []
-    in read . conc . digits xs $ n
+process n = sum . map (read . digits n) . lines
 
-digits :: [Int] -> Int -> [Int]
-digits xs 0 = []
-digits xs n = let
+digits :: Int -> String -> String
+digits 0 xs = []
+digits n xs = let
     digit = maximum . drop (n-1) . reverse $ xs
     remXs = drop 1 . dropWhile (/= digit) $ xs
-    in digit : digits remXs (n-1)
-
+    in digit : digits (n-1) remXs
