9dde7ffc47ebcf49c07d4a070ecb458c3c34f135
thomasstaud <thomas.staud05@gmail.com>
2025-12-08 11:44:33 +0100

    day 08 part 1 incorrect

--- day-08/day-08.hs
+++ day-08/day-08.hs
@@ -11,17 +11,17 @@ parse = let
         in (read x, read y, read . drop 1 $ z)
     in map parseLine . lines
 
-process :: [Coord] -> Int -> [[Coord]]
+process :: [Coord] -> Int -> [Int]
 process unsortedCoords n = let
     coords = sort (>) unsortedCoords
     manhattan ((a,b,c), (x,y,z)) = abs (a-x) + abs (b-y) + abs (c-z)
     combos = combinations coords
     measured = zip combos (map manhattan combos)
     sorted = sort (\ x y -> snd x > snd y) measured
-    connections = take n sorted
-    circuits = collectCircuits (map fst connections) coords
+    connections = map fst sorted
+    circuits = connectN n [[c] | c <- coords] connections
     largestCircuits = take 3 . sort (<) . map length $ circuits
-    in circuits
+    in largestCircuits
 
 sort :: (a -> a -> Bool) -> [a] -> [a]
 sort cmp [] = []
@@ -32,8 +32,18 @@ sort cmp (x:xs) = let
 combinations :: [Coord] -> [(Coord, Coord)]
 combinations coords = [(a, b) | a <- coords, b <- dropWhile (<=a) coords]
 
-collectCircuits :: [(Coord, Coord)] -> [Coord] -> [[Coord]]
-collectCircuits _ [] = []
-collectCircuits conns (c : coords) = let
-    (inside, outside) = partition (\ x -> (c, x) `elem` conns) coords
-    in (c : inside) : collectCircuits conns outside
+connectN :: Int -> [[Coord]] -> [(Coord, Coord)] -> [[Coord]]
+connectN 0 circuits _ = circuits
+connectN n circuits conns = let
+    ([conn], remConns) = splitAt 1 conns
+    (circuits', updated) = addConnection circuits conn
+    n' = if updated then n-1 else n
+    in connectN n' circuits' remConns
+
+addConnection :: [[Coord]] -> (Coord, Coord) -> ([[Coord]], Bool)
+addConnection circuits conn = let
+    c1 = head . filter (fst conn `elem`) $ circuits
+    c2 = head . filter (snd conn `elem`) $ circuits
+    connected = head c1 == head c2
+    circuits' = [c' | c <- circuits, c /= c2, let c' = if c /= c1 then c else c ++ c2]
+    in if connected then (circuits, False) else (circuits', True)
